0c969b54597be643f95f74f3b0cba98311427afe
JamieBurridge <xross_dev@proton.me>
2023-05-19 19:24:09 +0100

    added card creation

---
+++ src/js/data/cards_data.js
@@ -0,0 +1,16 @@
+const cardsData = [
+  {
+    name: "Coffee Machine",
+    description:
+      " A silly little coffee machine using css and minimal javascript.",
+    link: "https://jamieburridge.github.io/coffee-machine/",
+  },
+  {
+    name: "Dakee",
+    description:
+      "Concept for an app that helps tourists find what they need in a foreign country.",
+    link: "https://jamieburridge.github.io/coffee-machine/",
+  },
+];
+
+export default cardsData;
--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,42 @@
+import cardsData from "./data/cards_data.js";
+
 window.onload = () => {
-    const greetingEl = document.getElementById("home_greeting");
+  // Set greeting
+  const greetingEl = document.getElementById("home_greeting");
+  greetingEl.textContent = getTimeBasedMessage();
 
-    greetingEl.textContent = getTimeBasedMessage()
-
+  // Create portfolio cards
+  const portfolioGridEl = document.getElementById("portfolio_grid");
+  createPortfolioCards(cardsData, portfolioGridEl);
 };
 
 function getTimeBasedMessage() {
-    let time = new Date();
-    time = time.getHours();
+  let time = new Date();
+  time = time.getHours();
 
-    if (time >= 1 && time < 12) {
-        return "Good morning"
-    } else if (time >= 12 && time < 19) {
-        return "Good afternoon"
-    } else if (time >= 19) {
-        return "Good evening"
-    }
+  if (time >= 1 && time < 12) {
+    return "Good morning";
+  } else if (time >= 12 && time < 19) {
+    return "Good afternoon";
+  } else if (time >= 19) {
+    return "Good evening";
+  }
+}
+
+function createPortfolioCards(data, container) {
+  data.forEach((card) => {
+    const gridItemEl = document.createElement("div");
+    const projectHeading = document.createElement("h3");
+    const projectLink = document.createElement("a");
+    const projectDescription = document.createElement("p");
+
+    projectLink.textContent = card.name;
+    projectLink.href = card.link;
+    projectDescription.textContent = card.description;
+
+    projectHeading.appendChild(projectLink);
+    gridItemEl.appendChild(projectHeading);
+    gridItemEl.appendChild(projectDescription);
+    container.appendChild(gridItemEl);
+  });
 }
