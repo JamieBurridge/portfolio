697221f8128c1933266f02d64f4677a8b38404f6
JamieBurridge <xross_dev@proton.me>
2023-07-28 16:32:34 +0100

    Update portoflio

---
+++ src/data/projectsData.js
@@ -0,0 +1,49 @@
+import { languages } from "../helpers/languages";
+
+export const projectsData = [
+  {
+    heading: "4rums",
+    description: "4rums is a forum site made with PHP, Bootstrap and MySQL",
+    link: "https://github.com/JamieBurridge/4rums",
+    languages_used: [
+      languages.php,
+      languages.html,
+      languages.css,
+      languages.bootstrap,
+      languages.mysql,
+    ],
+  },
+  {
+    heading: "Rock, paper, scissors",
+    description:
+      "Rock, paper, scissors game made with Expo, a React framework for building web apps.",
+    link: "https://rock-paper-scissors-expo.vercel.app/",
+    languages_used: [languages.react, languages.html, languages.css],
+  },
+  {
+    heading: "Factopix",
+    description:
+      "Factopix is a website that generates a random fact with a random picture.",
+    link: "https://jamieburridge.github.io/Factopix/",
+    languages_used: [languages.html, languages.css, languages.javascript],
+  },
+  {
+    heading: "Coffee Machine",
+    description:
+      "An interactive coffee machine made. This was built mainly to practice SASS and CSS animations.",
+    link: "https://jamieburridge.github.io/coffee-machine/",
+    languages_used: [
+      languages.html,
+      languages.css,
+      languages.sass,
+      languages.javascript,
+    ],
+  },
+  {
+    heading: "Eternal Duel",
+    description:
+      "A command-line RPG where you fight againsts monsters, get rewards and level up your character.",
+    link: "https://github.com/JamieBurridge/eternal-duel",
+    languages_used: [languages.python],
+  },
+];
