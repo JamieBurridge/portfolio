3e223318b7784c5b74e506a96d63170041e83f03
JamieBurridge <xross_dev@proton.me>
2023-12-02 18:48:12 +0000

    add project categories and more projects

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -2,55 +2,90 @@ import { languages } from "../helpers/languages";
 
 export const projectsData = [
   {
-    heading: "Hyperstack",
-    description:
-      "Hyperstack is a Cloud computing platform. I was part of a team of 2 that worked on the front-end of this platform using NextJS and TailwindCSS.",
-    link: "https://www.hyperstack.cloud/",
-    languages_used: [languages.nextjs, languages.css],
-  },
-  {
-    heading: "4rums",
-    description: "4rums is a forum site made with PHP, Bootstrap and MySQL",
-    link: "https://github.com/JamieBurridge/4rums",
-    languages_used: [
-      languages.php,
-      languages.html,
-      languages.css,
-      languages.bootstrap,
-      languages.mysql,
+    sectionTitle: "Full-stack projects",
+    projects: [
+      {
+        heading: "Hyperstack",
+        description:
+          "Hyperstack is a Cloud computing platform. I was part of a team of 2 that worked on the front-end of this platform using NextJS and TailwindCSS.",
+        link: "https://www.hyperstack.cloud/",
+        languages_used: [languages.nextjs, languages.css],
+      },
+      {
+        heading: "4rums",
+        description: "4rums is a forum site made with PHP, Bootstrap and MySQL",
+        link: "https://github.com/JamieBurridge/4rums",
+        languages_used: [
+          languages.php,
+          languages.html,
+          languages.css,
+          languages.bootstrap,
+          languages.mysql,
+        ],
+      },
     ],
   },
-  // {
-  //   heading: "Rock, paper, scissors",
-  //   description:
-  //     "Rock, paper, scissors game made with Expo, a React framework for building web apps.",
-  //   link: "https://rock-paper-scissors-expo.vercel.app/",
-  //   languages_used: [languages.react, languages.html, languages.css],
-  // },
   {
-    heading: "Factopix",
-    description:
-      "Factopix is a website that generates a random fact with a random picture.",
-    link: "https://jamieburridge.github.io/Factopix/",
-    languages_used: [languages.html, languages.css, languages.javascript],
+    sectionTitle: "Front-end mini projects",
+    projects: [
+      {
+        heading: "Rock, paper, scissors",
+        description:
+          "Rock, paper, scissors game made with Expo, a React framework for building web apps.",
+        link: "https://rock-paper-scissors-expo.vercel.app/",
+        languages_used: [languages.react, languages.html, languages.css],
+      },
+      {
+        heading: "Factopix",
+        description:
+          "Factopix is a website that generates a random fact with a random picture.",
+        link: "https://jamieburridge.github.io/Factopix/",
+        languages_used: [languages.html, languages.css, languages.javascript],
+      },
+      {
+        heading: "Coffee Machine",
+        description:
+          "An interactive coffee machine made. This was built mainly to practice SASS and CSS animations.",
+        link: "https://jamieburridge.github.io/coffee-machine/",
+        languages_used: [
+          languages.html,
+          languages.css,
+          languages.sass,
+          languages.javascript,
+        ],
+      },
+    ],
   },
-  // {
-  //   heading: "Coffee Machine",
-  //   description:
-  //     "An interactive coffee machine made. This was built mainly to practice SASS and CSS animations.",
-  //   link: "https://jamieburridge.github.io/coffee-machine/",
-  //   languages_used: [
-  //     languages.html,
-  //     languages.css,
-  //     languages.sass,
-  //     languages.javascript,
-  //   ],
-  // },
-  // {
-  //   heading: "Eternal Duel",
-  //   description:
-  //     "A command-line RPG where you fight againsts monsters, get rewards and level up your character.",
-  //   link: "https://github.com/JamieBurridge/eternal-duel",
-  //   languages_used: [languages.python],
-  // },
+  {
+    sectionTitle: "CLI Projects",
+    projects: [
+      {
+        heading: "Binit",
+        description:
+          "A file trashing tool for Linux users to safely trash unwanted files.",
+        link: "https://github.com/JamieBurridge/binit",
+        languages_used: [languages.python],
+      },
+      {
+        heading: "Pycal",
+        description: "Pycal is a simple, lightweight list-style calendar.",
+        link: "https://github.com/JamieBurridge/pycal",
+        languages_used: [languages.python],
+      },
+      {
+        heading: "Tic Tac Toe",
+        description:
+          "Just a fun tic-tac-toe game made with Python. Play against a bot.",
+        link: "https://github.com/JamieBurridge/python-tic-tac-toe",
+        languages_used: [languages.python],
+      },
+      {
+        heading: "HTML Comment Finder",
+        description:
+          "A CLI tool made with Python to find all comments in html files.",
+        link: "https://github.com/JamieBurridge/html-comment-finder",
+        languages_used: [languages.python],
+      },
+    ],
+  },
 ];
